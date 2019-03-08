79147650a90e5d49980cd1c3b40a66a8786cbc90
admantiumboy <admantiumboy@gmail.com>
2019-03-08 16:29:00 +0530

    Added Encription

--- app/src/main/java/com/example/smarthealthcare/MainActivity.java
+++ app/src/main/java/com/example/smarthealthcare/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.smarthealthcare;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final String secretKey = "ssshhhhhhhhhhh!!!!";
+
+        String originalString = "howtodoinjava.com";
+        String encryptedString = Cryptography.encrypt(originalString, secretKey) ;
+        String decryptedString = Cryptography.decrypt(encryptedString, secretKey) ;
+
+        System.out.println("po--"+originalString);
+        System.out.println("po--"+encryptedString);
+        System.out.println("po--"+decryptedString);
     }
 }
